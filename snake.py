d187c5f3f50fd11a61c4c2d6b12daa3ed9e39092
Jeffrey Lin <jefreylin7411@gmail.com>
2021-01-09 12:25:35 -0500

    Improved code time complexity

--- snake.py
+++ snake.py
@@ -6,6 +6,32 @@ import math
 green = (0, 192, 0)
 red = (192, 0, 0)
 
+class Pair:
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+		
+class Node:
+	def __init__(self, pre, coor):
+		self.pre = pre
+		self.coor = coor
+		
+class Snake:
+	def __init__(self, head, tail, length):
+		self.head = head
+		self.tail = tail
+		self.length = length
+		
+	def removeTail(self):
+		self.tail = self.tail.pre
+		
+	def setHead(self, coor):
+		temp = Node(None, coor)
+		if self.length == 1:
+			self.tail.pre = temp
+		self.head.pre = temp
+		self.head = temp
+		
 def moveSnake(x, y):
     global running
     global snake
@@ -13,27 +39,27 @@ def moveSnake(x, y):
     global appleX
     global appleY
     global apple
-    global score
     global dead
-    a = snake[0][0]
-    b = snake[0][1]
+    a = snake.head.coor.x
+    b = snake.head.coor.y
     if a+x < 0 or a+x > 39 or b+y < 0 or b+y > 39 or grid[a+x][b+y] == 1:
         dead = True
         return
 
     if appleX == a+x and appleY == b+y:
         apple = False
-        score += 1
+        snake.length += 1
         font = pygame.font.SysFont('text.ttf', 45)
         pygame.draw.rect(screen, (0,0,0), (0, 601, 600, 50))
-        text = font.render(str(score), True, (255, 255, 255))
+        text = font.render(str(snake.length-1), True, (255, 255, 255))
         screen.blit(text, (10, 610))
+        snake.setHead(Pair(a+x, b+y)) 
     else:
-        temp = snake.pop()
-        pygame.draw.rect(screen, (0,0,0), (temp[0]*15+2, temp[1]*15+2, 12, 12))
-        grid[temp[0]][temp[1]] = 0
-        
-    snake.insert(0, [a + x, b + y]) 
+        snake.setHead(Pair(a+x, b+y))
+        temp = snake.tail.coor
+        snake.removeTail()
+        pygame.draw.rect(screen, (0,0,0), (temp.x*15+2, temp.y*15+2, 12, 12))
+        grid[temp.x][temp.y] = 0
     grid[a+x][b+y] = 1
     pygame.draw.rect(screen, green, ((a+x)*15+2, (b+y)*15+2, 12, 12))
 
@@ -56,14 +82,14 @@ def main():
     global grid
     global running
     global apple
-    global score
     global dead
     dead = False
-    score = 0
     apple = False
     x = 1
     y = 0
-    snake = [[20, 20]]
+    head = Node(None, Pair(20, 20))
+    tail = Node(head, Pair(20, 20))
+    snake = Snake(head, tail, 1)
     grid = [[0 for i in range(40)] for j in range(40)]
     grid[20][20] = 1
 
@@ -107,10 +133,11 @@ def start():
     screen = pygame.display.set_mode((600, 650))
     pygame.display.set_caption("Snake")
     screen.fill((0, 0, 0))
-
-    for i in range (1,40):
-        pygame.draw.line(screen, (255,255,255), (15*i, 0), (15*i,600), 1)
-        pygame.draw.line(screen, (255,255,255), (0, 15*i), (600,15*i), 1)
+	
+	# Uncomment to show grid lines
+    #for i in range (1,40):
+    #    pygame.draw.line(screen, (255,255,255), (15*i, 0), (15*i,600), 1)
+    #    pygame.draw.line(screen, (255,255,255), (0, 15*i), (600,15*i), 1)
     pygame.draw.line(screen, (255,255,255), (0, 600), (600,600), 1)
     font = pygame.font.SysFont('text.ttf', 45)
     text = font.render('0', True, (255, 255, 255))
